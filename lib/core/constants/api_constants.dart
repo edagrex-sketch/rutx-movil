9b2317f5c2539bb11b4c14f625a4a1377d8c0296
Neyser Manuel <estradaperezneyser@gmail.com>
2026-07-07 13:40:35 -0600

    feat: Finaliza lista y detalle de ventas. Implementa navegación, totales, indicadores de estado funcionales y estabiliza envío a Firebird incluyendo modo offline. (#11)

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -2,9 +2,13 @@ import 'dart:io';
 
 class ApiConstants {
   static bool useMock = false;
+  static bool useLocalIp = true; // Cambia a false si vuelves a usar el emulador (10.0.2.2)
 
   static String get baseUrl {
     final port = useMock ? '5048' : '5047';
+    if (useLocalIp) {
+      return 'http://192.168.100.61:$port';
+    }
     if (Platform.isAndroid) {
       return 'http://10.0.2.2:$port';
     }
